27f17fa4dc8ecda1c6858339ca1266bff36da7d1
kittyxcode <katherine.pino059@gmail.com>
2024-11-29 17:14:44 -0300

    feat(part1/courseinfo): update main.jsx, app.jsx and eslint config

--- Part1/CourseInfo/src/App.jsx
+++ Part1/CourseInfo/src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+const Header = (props) => {
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>{props.course}</h1>   
+    </>
+  )
+}
+const Content = (props) =>{
+  return(
+    <>
+      <Part parts={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part parts={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part parts={props.parts[2].name} exercises={props.parts[2].exercises}/>
+    </>
+  )
+}
+
+const Part =(props) => {
+  return(
+    <>
+      <p>
+        {props.parts} {props.exercises}
       </p>
     </>
+  )
+}
+
+const Total =(props)=>{
+  return(
+    <>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </>
+    
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name:'Half Stack application development',
+    parts:[
+    {
+      name:'Fundamentals of React',
+      exercises:10
+    },
+    {
+      name:'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name:'State of a component',
+      exercises:14
+    }
+  ]
+  }
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
   )
 }
 
