d358085ccd72b1064ee39c4396e4234bd9fa5a34
kittyxcode <katherine.pino059@gmail.com>
2025-01-14 14:51:57 -0300

    feat(Components): Add Part, Header, Content, and Total components for course structure

---
+++ Part2/CourseInfo/src/components/Content.jsx
@@ -0,0 +1,11 @@
+const Content = (props) =>{
+    return(
+      <>
+        <Part parts={props.parts[0].name} exercises={props.parts[0].exercises}/>
+        <Part parts={props.parts[1].name} exercises={props.parts[1].exercises}/>
+        <Part parts={props.parts[2].name} exercises={props.parts[2].exercises}/>
+      </>
+    )
+  }
+
+  export default Content
---
+++ Part2/CourseInfo/src/components/Part.jsx
@@ -0,0 +1,11 @@
+const Part =(props) => {
+    return(
+      <>
+        <p>
+          {props.parts} {props.exercises}
+        </p>
+      </>
+    )
+  }
+
+  export default Part
