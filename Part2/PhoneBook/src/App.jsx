f88c82603b5b88758ea34423dfb37456f9f0b1c6
kittyxcode <katherine.pino059@gmail.com>
2025-01-28 19:18:25 -0300

    feat(App 2.8 ok): Extend person object to include phone numbers and update form inputs

--- Part2/PhoneBook/src/App.jsx
+++ Part2/PhoneBook/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -12,7 +13,8 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     } else {
       const newPerson = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(newPerson))
       setNewName('')
@@ -28,11 +30,14 @@ const App = () => {
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={(e)=> setNewNumber(e.target.value)}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
