c4af4e89e73dcfc6119c86fdaae46a947e640056
kittyxcode <katherine.pino059@gmail.com>
2025-01-28 19:23:26 -0300

    feat(App): Add unique IDs to person objects and implement filter input for phonebook

--- Part2/PhoneBook/src/App.jsx
+++ Part2/PhoneBook/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -24,6 +27,9 @@ const App = () => {
   return (
     <div>
       <div>debug: {newName}</div>
+      <div>
+          Filter Show with: <input value={newNumber} onChange={(e)=> setNewNumber(e.target.value)}/>
+      </div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
