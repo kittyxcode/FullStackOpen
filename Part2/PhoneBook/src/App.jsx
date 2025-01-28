48b6a67342f5b5683ad1d92b272ed06576b70568
kittyxcode <katherine.pino059@gmail.com>
2025-01-28 18:52:40 -0300

    feat(App 2.7 ok): Implement addPerson function to handle name submissions and prevent duplicates

--- Part2/PhoneBook/src/App.jsx
+++ Part2/PhoneBook/src/App.jsx
@@ -6,13 +6,26 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const newPerson = {
+        name: newName
+      }
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+    }
+  }
+
   return (
     <div>
       <div>debug: {newName}</div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
