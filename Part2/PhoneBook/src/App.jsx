96481f99a37e5d8759f157b5e15de98fe40d610b
kittyxcode <katherine.pino059@gmail.com>
2025-01-30 16:44:04 -0300

    feat(App): Refactor form handling by creating PersonForm component

--- Part2/PhoneBook/src/App.jsx
+++ Part2/PhoneBook/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -37,17 +38,11 @@ const App = () => {
       
       <h2>Phonebook</h2>
       <Filter newSearch={newSearch} setNewSearch={setNewSearch}/>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={(e)=> setNewNumber(e.target.value)}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm addPerson={addPerson} 
+                  newName={newName} 
+                  newNumber={newNumber} 
+                  handleNameChange={setNewName}
+                  handleNumberChange={setNewNumber}/>
       <h2>Numbers</h2>
       {personsToShow.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
