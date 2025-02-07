72ce9815e40bbf39531c90e191133aa54b7c2bfa
kittyxcode <katherine.pino059@gmail.com>
2025-02-07 17:14:20 -0300

    fix: Remove unnecessary API call in main.jsx before rendering App component

--- Part2/PhoneBook/src/main.jsx
+++ Part2/PhoneBook/src/main.jsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom/client'
 import axios from 'axios'
 import App from './App'
 
-axios.get('http://localhost:3001/persons').then(response => {
-  ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-})
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
