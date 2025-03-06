351988fef8fb43c5f1684d32ae371c6038a51d50
kittyxcode <katherine.pino059@gmail.com>
2025-03-05 22:18:32 -0300

    chore: Remove unnecessary comment line in App component

--- Part2/Notes/src/App.jsx
+++ Part2/Notes/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     noteService.getAll().then((initialNotes) => {
       setNotes(initialNotes);
     });
-  }, []);//kkk
+  }, []);
 
   const addNote = (event) => {
     event.preventDefault();
