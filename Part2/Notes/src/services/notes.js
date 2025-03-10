d85de46f6a3a991928d6ddef6d7ad9b6f50abef3
kittyxcode <katherine.pino059@gmail.com>
2025-03-09 22:47:07 -0300

    chore: Remove unnecessary comment line in notes service

--- Part2/Notes/src/services/notes.js
+++ Part2/Notes/src/services/notes.js
@@ -6,7 +6,6 @@ const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }*/
-//
 const getAll = () => {
   const request = axios.get(baseUrl)
   const nonExisting = {
