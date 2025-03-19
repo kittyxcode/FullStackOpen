bc8df34dbbe157bd6ef408029f303d4e7f62d400
kittyxcode <katherine.pino059@gmail.com>
2025-03-19 20:59:25 -0300

    feat: Add state management for countries in App component

--- Part2/CountriesSelector/src/App.jsx
+++ Part2/CountriesSelector/src/App.jsx
@@ -1,8 +1,13 @@
+import {useState} from 'react';
+
 import InputCountry from './components/InputCountry';
-import {getAll, getOne} from './services/countries';
+//import { getAll, getOne } from './services/countries';
 
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+
   return (
     <div>
       find countries:  <InputCountry />
