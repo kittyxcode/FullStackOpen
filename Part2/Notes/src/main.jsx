f5c495ac21530f4fdaf42f650c56016566adb902
kittyxcode <katherine.pino059@gmail.com>
2025-03-18 22:07:53 -0300

    fix: Update API endpoint for notes service to use port 3002

--- Part2/Notes/src/main.jsx
+++ Part2/Notes/src/main.jsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom/client'
 import axios from 'axios'
 import App from './App'
 
-axios.get('http://localhost:3001/notes').then(response => {
+axios.get('http://localhost:3002/notes').then(response => {
   ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 })
