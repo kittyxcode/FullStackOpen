e3c4c3dcb90db581576bb92ac6eee94d80ae8a4a
kittyxcode <katherine.pino059@gmail.com>
2025-03-20 15:49:52 -0300

    feat: Implement country filtering and display details in InputCountry component

--- Part2/CountriesSelector/src/App.jsx
+++ Part2/CountriesSelector/src/App.jsx
@@ -6,19 +6,28 @@ import countries from './services/countries';
 
 function App() {
   const [countriesData, setCountriesData] = useState([]);
+  const [countriesToShow, setCountriesToShow] = useState([]);
+
+
   useEffect(() => {
     countries.getAll().then((initialCountries) => {
       setCountriesData(initialCountries);
+      //console.log(initialCountries);
     });
   }
   , []);
 
-  
-
+  const handleFilterChange = (event) => {
+    const filterValue = event.target.value;
+    const filteredCountries = countriesData.filter((country) => {
+      return country.name.common.toLowerCase().includes(filterValue.toLowerCase());
+    });
+    setCountriesToShow(filteredCountries);
+  }
 
   return (
     <div>
-      find countries:  <InputCountry />
+      find countries:  <InputCountry onChange={handleFilterChange} countriesTotal={countriesToShow}/>
     </div>
   );
 }
--- Part2/CountriesSelector/src/components/InputCountry.jsx
+++ Part2/CountriesSelector/src/components/InputCountry.jsx
@@ -1,4 +1,33 @@
-const InputCountry = ({ value, onChange }) => {
+const InputCountry = ({ value, onChange, countriesTotal }) => {
+  console.log(countriesTotal);
+  if(countriesTotal.length === 1) {
+    return (
+      <div>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          placeholder="Type a country"
+        />
+        <h2>{countriesTotal[0].name.common}</h2>
+        <p>capital {countriesTotal[0].capital}</p>
+        <p>population {countriesTotal[0].population}</p>
+        <h3>languages</h3>
+        <ul>
+          {Object.values(countriesTotal[0].languages).map((language, index) => {
+            return <li key={index}>{language}</li>;
+          })}
+        </ul>
+        <img
+          src={countriesTotal[0].flags.png}
+          alt={countriesTotal[0].name.common}
+          width="100"
+        />
+      </div>
+    );
+  }
+
+
   return (
     <input
       type="text"
