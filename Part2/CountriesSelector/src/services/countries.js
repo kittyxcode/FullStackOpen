cc4577a829f0e29f456e4fd09ef74e076c1b221c
kittyxcode <katherine.pino059@gmail.com>
2025-03-19 21:26:28 -0300

    feat: Remove unused getOne function from countries service

--- Part2/CountriesSelector/src/services/countries.js
+++ Part2/CountriesSelector/src/services/countries.js
@@ -6,9 +6,4 @@ const getAll = () => {
     return request.then(response => response.data)
 }
 
-const getOne = (name) => {
-    const request = axios.get(`${baseUrl}api/name/${name}`)
-    return request.then(response => response.data)
-}
-
-export default { getAll, getOne }
+export default { getAll}
